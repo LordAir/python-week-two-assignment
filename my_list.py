d46710e7e8b7101376500295a688b007341bd4cc
LordAir <136017173+LordAir@users.noreply.github.com>
2025-03-13 19:28:40 +0300

    Create my_list.py

---
+++ my_list.py
@@ -0,0 +1,26 @@
+my_list = []
+
+# Append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert
+my_list.insert(1, 15)
+
+# Extend
+my_list.extend([50, 60, 70])
+
+# Pop
+my_list.pop()
+
+# Sort
+my_list.sort()
+
+# Print the index (30)
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+# Print
+print("Final list:", my_list)
